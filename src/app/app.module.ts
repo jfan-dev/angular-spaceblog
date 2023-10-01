b8ce8d06c128eced2d8eb8e48101b54651dab963
jfan-dev <122750665+jfan-dev@users.noreply.github.com>
2023-10-01 10:05:13 -0300

    Normalize app component files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
